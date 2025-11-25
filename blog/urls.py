f6ebe18ff4639247945ae6fa9f80bccf2315106a
Ashley <alawrence22@ivytech.edu>
2025-11-24 19:09:35 -0500

    included blog url

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     # we can list all of our urls for our website here.. we will first start with the index or homepage
     path("/", views.post_list, name="post_list"),
-    
+    path('', include('blog.urls')), 
 ]
