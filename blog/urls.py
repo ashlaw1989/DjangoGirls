24844e2177785c234d1c0cdf865483f22013e601
Ashley <alawrence22@ivytech.edu>
2025-12-01 21:22:34 -0500

    module 5 ready

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     # we can list all of our urls for our website here.. we will first start with the index or homepage
     path("", views.post_list, name="post_list"),
+    path("post/<int:pk>", views.post_detail, name="post_detail"),
+    path("post/new", views.post_new, name="post_new"),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -10,3 +12,22 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     # when rendering a webpage we return a render of the request, athe html page we want to load, and any data we want to pass to that webpage (usually in the form of a db query)
     return render(request, "blog/post_list.html", {"posts" : posts})
+
+# see one post details
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post" : post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", pk=post.pk)
+
+    else:
+        form = PostForm()
+    return render(request, "blog/post_edit.html", {"form" : form})
